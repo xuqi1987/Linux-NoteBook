92d0a82135fe6eef20121fcdbbab175f0b110a18
XuQi-Ubuntu <xuqi.jx@gmail.com>
2019-04-30 10:20:48 +0800

    add

---
+++ Code-Snap/05_ARM/cmake-build-debug/CMakeFiles/03_ARM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03_ARM.dir/main.cpp.o"
+  "03_ARM.pdb"
+  "03_ARM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03_ARM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
