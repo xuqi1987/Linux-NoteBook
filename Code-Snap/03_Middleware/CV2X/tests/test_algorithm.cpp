2fbcd5b7bc0e3a4308842c4a6157563cc2a67ad5
xuqi1987 <xuqi.jx@gmail.com>
2019-08-23 00:17:22 +0800

    add

--- Code-Snap/03_Middleware/CV2X/tests/test_algorithm.cpp
+++ Code-Snap/03_Middleware/CV2X/tests/test_algorithm.cpp
@@ -7,12 +7,23 @@ using namespace v2x;
 int main()
 {
 
-    double dlon1 = 116.427800;
-    double dlat1 = 39.912462;
-    double dlon2 = 116.428038;
-    double dlat2 = 39.912458;
-    // 9299.65m
+    double lon_hv = 116.427800;
+    double lat_hv = 39.912462;
+    double lon_rv = 116.427800;
+    double lat_rv = 39.912658;
+    double speed_hv  = 30.0;
+    double speed_rv  = 66.0;
 
-    std::cout << V2xAlgorithm::Lonlat2dis(dlon1,dlat1,dlon2,dlat2) << "cm";
+    double heading_hv = 0;
+    double heading_rv = 0;
+
+    std::cout << "距离"<< V2xAlgorithm::Lonlat2dis(lon_hv,lat_hv,lon_rv,lat_rv) /100.0<< "m" << std::endl;
+
+    // 是否是正前方
+    std::cout <<"是否正前方" <<V2xAlgorithm::IsFront(lon_hv,lat_hv,0,lon_rv,lat_rv) <<std::endl;
+
+    bool isFCW = V2xAlgorithm::IsFWCWarning(lon_hv,lat_hv,heading_hv,speed_hv,lon_rv,lat_rv,heading_rv,speed_rv);
+    std::cout <<"是否会发生碰撞"<< isFCW<<std::endl;
+
     return 0;
 }
